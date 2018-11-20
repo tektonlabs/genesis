25ee59ee34e7321afbde2d1cd0769a190cf5559a
Joca Pereyra <jpereyraleon@gmail.com>
2018-11-20 18:40:56 -0500

    Fix format

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const yaml = require("js-yaml")
-const gitClone = require('git-clone');
+const yaml = require("js-yaml");
+const gitClone = require("git-clone");
 const inquirer = require("inquirer");
 
 const appDir = `${__dirname}/../`;
 const currDir = process.cwd();
-const config = yaml.safeLoad(fs.readFileSync(`${appDir}/config.yaml`, 'utf8'));
+const config = yaml.safeLoad(fs.readFileSync(`${appDir}/config.yaml`, "utf8"));
 const QUESTIONS = [
   {
     name: "project-name",
     type: "input",
     message: "Project name:",
-    validate: (input) => {
+    validate: input => {
       if (/^([A-Za-z][A-Za-z\-\_\d]*)$/.test(input)) return true;
       else
         return "Project name should starts with a letter and may only include letters, numbers, underscores or dashes.";
@@ -24,7 +24,7 @@ const QUESTIONS = [
     type: "list",
     message: "Which generator did you want to use as template?",
     choices: Object.keys(config),
-  }
+  },
 ];
 
 main();
@@ -34,18 +34,19 @@ async function main() {
   const projectName = answers["project-name"];
   const generatorName = answers["generator"];
 
-  if (! Array.isArray(config[generatorName])) {
-    // gitClone(config[generatorName].repository, `${currDir}/${projectName}`, {shallow: true});
+  if (!Array.isArray(config[generatorName])) {
+    gitClone(config[generatorName].repository, `${currDir}/${projectName}`, { shallow: true });
   } else {
-    var answer = await inquirer.prompt([{
+    var answer = await inquirer.prompt([
+      {
         name: "generator",
         type: "list",
         message: `Which ${generatorName} configuration do you want?`,
-        choices: config[generatorName].map(o => o.name ),
-    }]);
+        choices: config[generatorName].map(o => o.name),
+      },
+    ]);
 
-    var generator = config[generatorName].find(o => o.name == answer['generator']);
-    gitClone(generator.repository, `${currDir}/${projectName}`, {shallow: true});
+    var generator = config[generatorName].find(o => o.name == answer["generator"]);
+    gitClone(generator.repository, `${currDir}/${projectName}`, { shallow: true });
   }
-
 }
